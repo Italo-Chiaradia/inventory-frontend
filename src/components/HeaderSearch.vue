d741c6fa4e201a47ca7b4110aefb61923750b78c
italo-ch <italo_ide@id.uff.br>
2024-11-15 23:41:07 -0300

    feature search

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -2,13 +2,13 @@
   <header class="w-full py-8 bg-slate-800 text-white">
     <div class="max-w-screen-lg h-full m-auto items-center flex gap-4 px-4">
       <div class="flex-1 relative">
-        <input class="w-full h-full p-3 pl-10 rounded-full text-black focus:ring-yellow-500 focus:outline-none focus:ring-2" type="text" placeholder="Pesquisar produto">
+        <input v-model="value" @input="$emit('search', value)" class="w-full h-full p-3 pl-10 rounded-full text-black focus:ring-yellow-500 focus:outline-none focus:ring-2" type="text" placeholder="Pesquisar produto">
         <PhMagnifyingGlass :size="24" class="absolute top-3 left-3 text-gray-400" /> 
       </div>
       <RouterLink to="/new-product" class="bg-yellow-500 p-2 px-4 rounded-full font-bold hover:bg-opacity-80 text-md transition-all">
         Adicionar produto
       </RouterLink>
-      <button title="Sair" @click="handleLogOut">
+      <button title="Sair" @click="handleLogout">
         <PhPower :size="32" class="hover:opacity-50 transition-all"/>
       </button>
     </div>
@@ -16,5 +16,16 @@
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
   import {PhPower, PhMagnifyingGlass } from '@phosphor-icons/vue'
+  
+  const value = ref('')
+
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    localStorage.removeItem(':inventory-user')
+    router.push('/login')
+  }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
-  <HeaderSearch/>
+  <HeaderSearch @search="updateFilter"/>
   <main class="max-w-screen-lg m-auto mt-16 flex flex-wrap gap-x-4 gap-y-4 px-4">
-    <CardProduct v-for="product in state.products" :key="product.id" :product="product" />
+    <CardProduct v-for="product in filteredProducts" :key="product.id" :product="product" />
   </main>
 </template>
 
@@ -9,12 +9,21 @@
   import axios from "axios";
   import CardProduct from '@/components/CardProduct.vue';
   import HeaderSearch from '@/components/HeaderSearch.vue';
-  import { reactive, onMounted } from "vue";
+  import { computed, reactive, onMounted } from "vue";
 
   const state = reactive({
+    filter: '',
     products: [],
     isLoading: true
   })
+
+  const filteredProducts = computed(() => {
+    return state.products.filter(product => product.name.toLowerCase().includes(state.filter))
+  })
+
+  const updateFilter = (newFilter) => {
+    state.filter = newFilter.toLowerCase()
+  }
 
   onMounted(async () => {
     try {
